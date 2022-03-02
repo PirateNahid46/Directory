ed3a0b0f65717a9dedec5e926edc6c64e1d27461
Nahid <nhdnahid2022@gmail.com>
2022-03-02 22:28:35 +0600

    init

--- script.js
+++ script.js
@@ -10,35 +10,36 @@ const firebaseConfig = {
   firebase.initializeApp(firebaseConfig);
   const db = firebase.database();
   
-// const fetchChat = db.ref("info/");
-// fetchChat.on("child_added", function (snapshot) {
-//   const messages = snapshot.val();
-//     const msg = "<div class=\"cadetno\"> <a href=\"./Next/details.html?"+messages.cn+"\"> "+ messages.cn +" <a/></div>";
-//     document.getElementById("list").innerHTML += msg;
-// });
 
 
 document.getElementById("authform").addEventListener("submit", getname);
   function getname(e){
     e.preventDefault();
     const name = document.getElementById("namebox");
-    const chatTxt = document.getElementById("chat-txt");
+    const cn = document.getElementById("cn");
     const batch = document.getElementById("batch");
     const house = document.getElementById("house");
     const contact = document.getElementById("contact");
     const work = document.getElementById("work");
     const home = document.getElementById("home");
     const district = document.getElementById("district");
+    const misc = document.getElementById("misc");
+    const email = document.getElementById("email");
     const username = name.value;
-    const usercn = chatTxt.value;
+    const usercn = cn.value;
     const userbatch = batch.value;
     const userhouse = house.value;
     const usercontact = contact.value;
     const userwork = work.value;
     const userhome = home.value;
     const userdistrict = district.value;
+    const usermisc = misc.value;
+    const useremail = email.value;
   name.value = "";
-  chatTxt.value = "";
+  cn.value = "";
+  
+
+
       db.ref("info/" + usercn).set({
         name: username,
         cn: usercn,
@@ -48,6 +49,8 @@ document.getElementById("authform").addEventListener("submit", getname);
         work: userwork,
         home: userhome,
         district: userdistrict,
+        email: useremail,
+        misc : usermisc,
       });
     
     
